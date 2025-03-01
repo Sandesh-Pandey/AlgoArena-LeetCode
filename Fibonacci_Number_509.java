8ba2b2a09aca0e90307f995c76b69d79fef43afa
Sandesh Pandey <157128532+Sandesh-Pandey@users.noreply.github.com>
2025-03-01 21:50:23 +0530

    Create Fibonacci_Number_509.java

---
+++ Fibonacci_Number_509.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fib(int n) {
+       if(n==0){
+        return 0;
+       }else if(n==1){
+        return 1;
+       }
+
+       int ft = 0;                           // first_term (ft)
+       int st = 1;                           // second_term (st)
+
+       for(int i=1; i<=n; i++){
+        int tt = ft + st;                   // third_term (tt)
+
+        ft = st;
+        st = tt;
+           
+       }
+       
+       return ft;
+    }
+}
